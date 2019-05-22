19c62a3b6cbc0f9e59dbc27fb8c7b04a6ba1613e
Zazsona <40435178+Zazsona@users.noreply.github.com>
2019-05-22 23:03:35 +0100

    Add Eightball.

---
+++ Toys/EightBall/src/main/java/EightBall.java
@@ -0,0 +1,16 @@
+import commands.CmdUtil;
+import commands.Command;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+
+import java.util.Random;
+
+public class EightBall extends Command
+{
+    @Override
+    public void run(GuildMessageReceivedEvent msgEvent, String... parameters)
+    {
+        String[] Statements = { "It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy. Try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful." };
+        Random r = new Random();
+        msgEvent.getChannel().sendMessage(Statements[r.nextInt(Statements.length)]).queue();
+    }
+}
