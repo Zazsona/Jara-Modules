2db800c2bae2e917d8fa95f2655e5439b956d1f7
Zazsona <40435178+Zazsona@users.noreply.github.com>
2019-05-22 23:04:21 +0100

    Add Meta.

---
+++ Toys/Meta/src/main/java/Meta.java
@@ -0,0 +1,19 @@
+import commands.CmdUtil;
+import commands.Command;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+
+public class Meta extends Command
+{
+    @Override
+    public void run(GuildMessageReceivedEvent msgEvent, String... parameters)
+    {
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setDescription("```M  E  T  A\r\n\r\n" +
+                                            "E\r\n\r\n" +
+                                            "T\r\n\r\n" +
+                                            "A```");
+        embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+        msgEvent.getChannel().sendMessage(embed.build()).queue();
+    }
+}
