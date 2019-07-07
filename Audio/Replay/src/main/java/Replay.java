ba490805fae34e32a3d1691ec391495f0fd6ef3a
Zazsona <40435178+Zazsona@users.noreply.github.com>
2019-07-07 22:31:13 +0100

    Make replay work harder to find something to play.
    This makes replay look at the track history to find something to play, making it so users can replay a track once it has ended.
    This also adds the option for users to manually specify whether to replay the current or previous track.

--- Audio/Replay/src/main/java/Replay.java
+++ Audio/Replay/src/main/java/Replay.java
@@ -1,3 +1,4 @@
+import audio.Audio;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import commands.CmdUtil;
 import commands.Command;
@@ -9,19 +10,60 @@ public class Replay extends Command
     @Override
     public void run(GuildMessageReceivedEvent msgEvent, String... parameters)
     {
-        AudioPlayer player = CmdUtil.getGuildAudio(msgEvent.getGuild().getId()).getPlayer();
-        if (player.getPlayingTrack() != null)
+        Audio audio = CmdUtil.getGuildAudio(msgEvent.getGuild().getId());
+        AudioPlayer player = audio.getPlayer();
+        if (parameters.length <= 1)
         {
-            CmdUtil.getGuildAudio(msgEvent.getGuild().getId()).playWithFeedback(msgEvent.getMember(), player.getPlayingTrack().getInfo().uri, msgEvent.getChannel());
+            if (audio.getTrackHistory().size() > 0)
+            {
+                CmdUtil.getGuildAudio(msgEvent.getGuild().getId()).playWithFeedback(msgEvent.getMember(), audio.getTrackHistory().get(audio.getTrackHistory().size()-1).getInfo().uri, msgEvent.getChannel());
+            }
+            else
+            {
+                EmbedBuilder embed = new EmbedBuilder();
+                embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+                embed.setThumbnail("https://i.imgur.com/wHdSqH5.png");
+                embed.setTitle("Now Playing...");
+                embed.setDescription("There is no track to replay.");
+                msgEvent.getChannel().sendMessage(embed.build()).queue();
+            }
         }
         else
         {
-            EmbedBuilder embed = new EmbedBuilder();
-            embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
-            embed.setThumbnail("https://i.imgur.com/wHdSqH5.png");
-            embed.setTitle("Now Playing...");
-            embed.setDescription("No track is currently playing.");
-            msgEvent.getChannel().sendMessage(embed.build()).queue();
+            if (parameters[1].equalsIgnoreCase("current") || parameters[1].equalsIgnoreCase("this"))
+            {
+                if (player.getPlayingTrack() != null)
+                {
+                    CmdUtil.getGuildAudio(msgEvent.getGuild().getId()).playWithFeedback(msgEvent.getMember(), player.getPlayingTrack().getInfo().uri, msgEvent.getChannel());
+                }
+                else
+                {
+                    EmbedBuilder embed = new EmbedBuilder();
+                    embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+                    embed.setThumbnail("https://i.imgur.com/wHdSqH5.png");
+                    embed.setTitle("Now Playing...");
+                    embed.setDescription("There is no currently playing track to replay.");
+                    msgEvent.getChannel().sendMessage(embed.build()).queue();
+                }
+            }
+            else if (parameters[1].equalsIgnoreCase("last") || parameters[1].equalsIgnoreCase("previous"))
+            {
+                if (audio.getTrackHistory().size() >= 2)
+                {
+                    int indexOffset = (audio.isAudioPlayingInGuild()) ? 2 : 1;
+                    CmdUtil.getGuildAudio(msgEvent.getGuild().getId()).playWithFeedback(msgEvent.getMember(), audio.getTrackHistory().get(audio.getTrackHistory().size()-indexOffset).getInfo().uri, msgEvent.getChannel());
+                }
+                else
+                {
+                    EmbedBuilder embed = new EmbedBuilder();
+                    embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+                    embed.setThumbnail("https://i.imgur.com/wHdSqH5.png");
+                    embed.setTitle("Now Playing...");
+                    embed.setDescription("There is no previous track to replay.");
+                    msgEvent.getChannel().sendMessage(embed.build()).queue();
+                }
+            }
         }
+
     }
 }
