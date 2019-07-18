fe3035b50c7d45d16d65c98d84ee67393e198144
Zazsona <40435178+Zazsona@users.noreply.github.com>
2019-07-18 18:41:57 +0100

    Add the ability to select a channel with /say
    Day #Y under no internet conditions, modules continue to go untested, but they seem to be compiling.

--- Toys/Say/src/main/java/Say.java
+++ Toys/Say/src/main/java/Say.java
@@ -1,8 +1,10 @@
+import commands.CmdUtil;
 import commands.Command;
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
-
-import java.util.Random;
+import java.util.List;
 
 public class Say extends Command
 {
@@ -11,12 +13,41 @@ public class Say extends Command
     {
         if (parameters.length > 1)
         {
+            TextChannel channel = getChannel(msgEvent.getMessage());
             StringBuilder message = new StringBuilder();
             for (int i = 1; i<parameters.length; i++)
             {
                 message.append(parameters[i]).append(" ");
             }
-            msgEvent.getChannel().sendMessage(message.toString()).queue();
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+            embed.setDescription(message.toString());
+            channel.sendMessage(embed.build()).queue();
         }
+        else
+        {
+            CmdUtil.sendHelpInfo(msgEvent, getClass());
+        }
+    }
+
+    private TextChannel getChannel(Message message)
+    {
+        List<TextChannel> channels = message.getMentionedChannels();
+        if (channels.size() > 0)
+        {
+            return channels.get(0);
+        }
+        else if (message.getContentDisplay().length() > 2)
+        {
+            String channelName = message.getContentDisplay().split(" ")[2];
+            for (TextChannel channel : message.getGuild().getTextChannels())
+            {
+                if (channel.getName().equalsIgnoreCase(channelName))
+                {
+                    return channel;
+                }
+            }
+        }
+        return message.getTextChannel();
     }
 }
