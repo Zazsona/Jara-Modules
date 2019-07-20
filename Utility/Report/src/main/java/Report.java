cd7d146676888bd8d3a2d53882f7be24454c20b1
Zazsona <40435178+Zazsona@users.noreply.github.com>
2019-07-20 12:12:45 +0100

    Add Uptime to Report
    Day #Z under no internet conditions, commands continue to go untested. My smart shaver no longer sings merry christmas while harvesting all my personal data.
    The struggle continues.

--- Utility/Report/src/main/java/Report.java
+++ Utility/Report/src/main/java/Report.java
@@ -3,6 +3,7 @@ import commands.Command;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
+import java.lang.management.ManagementFactory;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
@@ -15,6 +16,7 @@ public class Report extends Command
         StringBuilder reportSB = new StringBuilder();
         reportSB.append("Bot User: "+msgEvent.getJDA().getSelfUser().getName()+"#"+msgEvent.getJDA().getSelfUser().getDiscriminator()+"\n");
         reportSB.append("Status: Online\n");
+        reportSB.append("Uptime: "+(ManagementFactory.getRuntimeMXBean().getUptime()/1000/60/60)+" Hours\n");
         reportSB.append("DateTime: " + OffsetDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm:ss")) + "(UTC) \n");
         reportSB.append("Shard: "+msgEvent.getJDA().getShardInfo().getShardId()+"\n");
         reportSB.append("Shard Total: "+msgEvent.getJDA().getShardInfo().getShardTotal()+"\n");
@@ -32,5 +34,10 @@ public class Report extends Command
         embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
 
         msgEvent.getChannel().sendMessage(embed.build()).queue();
+    }
+
+    private int getUptimeHours()
+    {
+        return (int)
     }
 }
