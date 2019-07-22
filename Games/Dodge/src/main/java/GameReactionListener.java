2aae0c035a70545ab69841c6a6f00c2a7816d582
Zazsona <40435178+Zazsona@users.noreply.github.com>
2019-07-22 02:57:48 +0100

    Add Dodge game.

---
+++ Games/Dodge/src/main/java/GameReactionListener.java
@@ -0,0 +1,38 @@
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class GameReactionListener extends ListenerAdapter
+{
+    Dodge gameInstance;
+    Message message;
+    Member player;
+    public GameReactionListener(Dodge gameInstance, Message message, Member player)
+    {
+        this.gameInstance = gameInstance;
+        this.message = message;
+        this.player = player;
+    }
+
+
+    @Override
+    public void onMessageReactionAdd(MessageReactionAddEvent mra)
+    {
+        if (mra.getMessageId().equals(message.getId()) && mra.getMember().equals(player))
+        {
+            if (mra.getReactionEmote().getName().equals("\u25B6"))
+            {
+                gameInstance.plotPlayer(false);
+            }
+            else if (mra.getReactionEmote().getName().equals("\u25C0"))
+            {
+                gameInstance.plotPlayer(true);
+            }
+        }
+        if (mra.getMessageId().equals(message.getId()) && !mra.getMember().equals(message.getGuild().getSelfMember()))
+        {
+            mra.getReaction().removeReaction(mra.getUser()).queue();
+        }
+    }
+}
