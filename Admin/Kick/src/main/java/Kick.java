8b3839e2a23d3e0aa605839fbbe5e84d912438b0
Zazsona <40435178+Zazsona@users.noreply.github.com>
2019-07-28 15:08:57 +0100

    Improve feedback and add reasons to Kick
    - Add kick reasons which show in the Audit Log
    - Notify users how many members were kicked
    - Notify users that, when some members can't be kicked, the others were.
    - Notify users if the bot lacks permission to perform any kicks
    - Added example usage to help page.

--- Admin/Kick/src/main/java/Kick.java
+++ Admin/Kick/src/main/java/Kick.java
@@ -1,6 +1,7 @@
 import commands.CmdUtil;
 import commands.Command;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
@@ -15,53 +16,88 @@ public class Kick extends Command
     @Override
     public void run(GuildMessageReceivedEvent msgEvent, String... parameters)
     {
-        List<Member> membersToKick = msgEvent.getMessage().getMentionedMembers();
-        if (membersToKick.size() >= 1)
+        if (msgEvent.getMember().getPermissions().contains(Permission.KICK_MEMBERS))
         {
-            StringBuilder feedbackMessage = new StringBuilder();
-            for (Member member : membersToKick)
+            List<Member> membersToKick = msgEvent.getMessage().getMentionedMembers();
+            parameters = msgEvent.getMessage().getContentRaw().split(" ");
+            String kickMessage = null;
+            if (parameters.length > membersToKick.size()+1)
             {
-                if (!member.equals(msgEvent.getGuild().getSelfMember()))
+                kickMessage = getKickReason(parameters, membersToKick.size());
+            }
+            if (membersToKick.size() >= 1)
+            {
+                StringBuilder feedbackMessage = new StringBuilder();
+                int kickCount = executeKicks(msgEvent, membersToKick, kickMessage, feedbackMessage);
+                if (feedbackMessage.length() > 0)
                 {
-                    try
+                    feedbackMessage.insert(0, "**Unable to kick members:**\n");
+                    if (kickCount > 0)
                     {
-                        msgEvent.getGuild().getController().kick(member).queue();
-                    }
-                    catch (PermissionException e)
-                    {
-                        buildKickErrorMessage(feedbackMessage, member, "Insufficient Permissions");
-                    }
-                    catch (IllegalArgumentException e)
-                    {
-                        buildKickErrorMessage(feedbackMessage, member, "Unknown Member");
+                        feedbackMessage.insert(0, "Successfully kicked "+kickCount+" members.\n\n");
                     }
                 }
                 else
                 {
-                    buildKickErrorMessage(feedbackMessage, member, "It's me");
+                    feedbackMessage.append("Successfully kicked ").append(kickCount).append(" members.");
                 }
-
-            }
-            if (feedbackMessage.length() > 0)
-            {
-                feedbackMessage.insert(0, "**Unable to kick users:**\n");
+                EmbedBuilder embed = new EmbedBuilder();
+                embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+                embed.setDescription(feedbackMessage.toString());
+                msgEvent.getChannel().sendMessage(embed.build()).queue();
             }
             else
             {
-                feedbackMessage.append("Members kicked successfully.");
+                CmdUtil.sendHelpInfo(msgEvent, getClass());
             }
-            EmbedBuilder embed = new EmbedBuilder();
-            embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
-            embed.setDescription(feedbackMessage.toString());
-            msgEvent.getChannel().sendMessage(embed.build()).queue();
         }
         else
         {
-            CmdUtil.sendHelpInfo(msgEvent, getClass());
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+            embed.setDescription("This bot does not have permission to kick members.");
+            msgEvent.getChannel().sendMessage(embed.build()).queue();
         }
     }
 
-    private void buildKickErrorMessage(StringBuilder feedbackMessage, Member member, String reasonForError)
+    private int executeKicks(GuildMessageReceivedEvent msgEvent, List<Member> membersToKick, String kickMessage, StringBuilder feedbackMessage)
+    {
+        int kickCount = 0;
+        for (Member member : membersToKick)
+        {
+            if (!member.equals(msgEvent.getGuild().getSelfMember()))
+            {
+                try
+                {
+                    if (kickMessage == null)
+                    {
+                        msgEvent.getGuild().getController().kick(member).queue();
+                        kickCount++;
+                    }
+                    else
+                    {
+                        msgEvent.getGuild().getController().kick(member, kickMessage).queue();
+                        kickCount++;
+                    }
+                }
+                catch (PermissionException e)
+                {
+                    getKickErrorMessage(feedbackMessage, member, "Insufficient Permissions");
+                }
+                catch (IllegalArgumentException e)
+                {
+                    getKickErrorMessage(feedbackMessage, member, "Unknown Member");
+                }
+            }
+            else
+            {
+                getKickErrorMessage(feedbackMessage, member, "It's me");
+            }
+        }
+        return kickCount;
+    }
+
+    private void getKickErrorMessage(StringBuilder feedbackMessage, Member member, String reasonForError)
     {
         if (member.getEffectiveName().equalsIgnoreCase(member.getUser().getName()))
         {
@@ -73,5 +109,15 @@ public class Kick extends Command
         }
         feedbackMessage.append(" - ").append(reasonForError);
         feedbackMessage.append("\n");
+    }
+
+    private String getKickReason(String[] parameters, int membersCount)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = (membersCount+1); i<parameters.length; i++)
+        {
+            sb.append(parameters[i]).append(" ");
+        }
+        return sb.toString().trim();
     }
 }
