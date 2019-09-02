c0b23eae2b05fb80a6c0c6a4f88bd49dc21f5725
Zazsona <40435178+Zazsona@users.noreply.github.com>
2019-09-02 17:22:43 +0100

    Add PlayHistory

---
+++ Audio/PlayHistory/src/main/java/PlayHistory.java
@@ -0,0 +1,49 @@
+import audio.Audio;
+import audio.ScheduledTrack;
+import commands.CmdUtil;
+import commands.Command;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+
+public class PlayHistory extends Command
+{
+    @Override
+    public void run(GuildMessageReceivedEvent msgEvent, String... parameters)
+    {
+        Audio audio = CmdUtil.getGuildAudio(msgEvent.getGuild().getId());
+        int trackCount = (audio.getTrackHistory().size() > 10) ? 10 : audio.getTrackHistory().size();
+        StringBuilder trackListBuilder = new StringBuilder();
+        StringBuilder entryBuilder = new StringBuilder();
+        if (trackCount > 0)
+        {
+            for (int i = 1; i<(trackCount+1); i++)
+            {
+                ScheduledTrack st = audio.getTrackHistory().get(audio.getTrackHistory().size()-i);
+                entryBuilder.append("**").append(st.getAudioTrack().getInfo().title).append("**\n");
+                entryBuilder.append(msgEvent.getGuild().getMemberById(st.getUserID()).getEffectiveName()).append("\n");
+                entryBuilder.append(st.getAudioTrack().getInfo().uri).append("\n");
+                if (trackListBuilder.length() + entryBuilder.length() < 1020)
+                {
+                    trackListBuilder.append(entryBuilder.toString()).append("\n");
+                    entryBuilder.setLength(0);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            trackListBuilder.append("No audio has been played.");
+        }
+
+
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+        embed.setThumbnail("https://i.imgur.com/wHdSqH5.png");
+        embed.setDescription(trackListBuilder.toString());
+        msgEvent.getChannel().sendMessage(embed.build()).queue();
+
+    }
+}
