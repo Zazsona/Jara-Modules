6d206b0894df747901e7a5d7ddd26aa058a23b1b
Zazsona <40435178+Zazsona@users.noreply.github.com>
2019-09-04 01:54:20 +0100

    Add multi-queuing to Replay.

--- Audio/Replay/src/main/java/Replay.java
+++ Audio/Replay/src/main/java/Replay.java
@@ -1,8 +1,12 @@
 import audio.Audio;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import commands.CmdUtil;
-import commands.Command;
+import configuration.SettingsUtil;
+import module.Command;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 public class Replay extends Command
@@ -12,58 +16,86 @@ public class Replay extends Command
     {
         Audio audio = CmdUtil.getGuildAudio(msgEvent.getGuild().getId());
         AudioPlayer player = audio.getPlayer();
-        if (parameters.length <= 1)
-        {
-            if (audio.getTrackHistory().size() > 0)
-            {
-                CmdUtil.getGuildAudio(msgEvent.getGuild().getId()).playWithFeedback(msgEvent.getMember(), audio.getTrackHistory().get(audio.getTrackHistory().size()-1).getInfo().uri, msgEvent.getChannel());
-            }
-            else
-            {
-                EmbedBuilder embed = new EmbedBuilder();
-                embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
-                embed.setThumbnail("https://i.imgur.com/wHdSqH5.png");
-                embed.setTitle("Now Playing...");
-                embed.setDescription("There is no track to replay.");
-                msgEvent.getChannel().sendMessage(embed.build()).queue();
-            }
-        }
-        else
+        if (parameters.length > 1)
         {
             if (parameters[1].equalsIgnoreCase("current") || parameters[1].equalsIgnoreCase("this"))
             {
                 if (player.getPlayingTrack() != null)
                 {
-                    CmdUtil.getGuildAudio(msgEvent.getGuild().getId()).playWithFeedback(msgEvent.getMember(), player.getPlayingTrack().getInfo().uri, msgEvent.getChannel());
+                    AudioTrack track = player.getPlayingTrack();
+                    queueTrackForReplay(msgEvent.getMember(), msgEvent.getChannel(), audio, player, track, parameters);
                 }
                 else
                 {
-                    EmbedBuilder embed = new EmbedBuilder();
-                    embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
-                    embed.setThumbnail("https://i.imgur.com/wHdSqH5.png");
-                    embed.setTitle("Now Playing...");
-                    embed.setDescription("There is no currently playing track to replay.");
-                    msgEvent.getChannel().sendMessage(embed.build()).queue();
+                    sendReplayMessage(msgEvent, "There is no currently playing track to replay.");
                 }
+                return;
             }
             else if (parameters[1].equalsIgnoreCase("last") || parameters[1].equalsIgnoreCase("previous"))
             {
-                if (audio.getTrackHistory().size() >= 2)
+                if (audio.getTrackHistory().size() > 0)
                 {
-                    int indexOffset = (audio.isAudioPlayingInGuild()) ? 2 : 1;
-                    CmdUtil.getGuildAudio(msgEvent.getGuild().getId()).playWithFeedback(msgEvent.getMember(), audio.getTrackHistory().get(audio.getTrackHistory().size()-indexOffset).getInfo().uri, msgEvent.getChannel());
+                    int indexOffset = 1;
+                    AudioTrack track = audio.getTrackHistory().get(audio.getTrackHistory().size()-indexOffset).getAudioTrack();
+                    queueTrackForReplay(msgEvent.getMember(), msgEvent.getChannel(), audio, player, track, parameters);
                 }
                 else
                 {
-                    EmbedBuilder embed = new EmbedBuilder();
-                    embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
-                    embed.setThumbnail("https://i.imgur.com/wHdSqH5.png");
-                    embed.setTitle("Now Playing...");
-                    embed.setDescription("There is no previous track to replay.");
-                    msgEvent.getChannel().sendMessage(embed.build()).queue();
+                    sendReplayMessage(msgEvent, "There is no previous track to replay.");
                 }
+                return;
             }
         }
+        if (audio.getTrackHistory().size() > 0 || audio.isAudioPlayingInGuild())
+        {
+            AudioTrack track = (audio.isAudioPlayingInGuild()) ? player.getPlayingTrack() : audio.getTrackHistory().get(audio.getTrackHistory().size()-1).getAudioTrack();
+            queueTrackForReplay(msgEvent.getMember(), msgEvent.getChannel(), audio, player, track, parameters);
+        }
+        else
+        {
+            sendReplayMessage(msgEvent, "There is no track to replay.");
+        }
 
+    }
+
+    private void queueTrackForReplay(Member member, TextChannel channel, Audio audio, AudioPlayer player, AudioTrack track, String[] parameters)
+    {
+        /*
+                Here we limit the queuing to the user's remaining queue space if it is smaller than the amount specified.
+                While this may seem redundant, as Jara blocks additional queue requests, it isn't.
+
+                This is because if hundreds of replays are requested, it may take multiple minutes to load the tracks, long enough for the first track to end.
+                When this track does end, then the user has a spot free queue spot, and this will instantly be filled if tracks are still being loaded in, effectively allowing them to block the queue.
+         */
+        int replayCount = getReplayCount(parameters);
+        int remainingQueue = SettingsUtil.getGuildSettings(channel.getGuild().getId()).getAudioQueueLimit(member)-audio.getUserQueueQuantity().get(member.getUser().getId());
+        int replaysToQueue = (replayCount < remainingQueue) ? replayCount : remainingQueue;
+        audio.playWithFeedback(member, track.getInfo().uri, channel);
+        for (int i = 1; i <replaysToQueue ; i++)
+        {
+            audio.play(member, player.getPlayingTrack().getInfo().uri);
+        }
+    }
+
+    private void sendReplayMessage(GuildMessageReceivedEvent msgEvent, String s)
+    {
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+        embed.setThumbnail("https://i.imgur.com/wHdSqH5.png");
+        embed.setTitle("Now Playing...");
+        embed.setDescription(s);
+        msgEvent.getChannel().sendMessage(embed.build()).queue();
+    }
+
+    private int getReplayCount(String[] parameters)
+    {
+        if (parameters[parameters.length-1].matches("[0-9]+"))
+        {
+            return Integer.parseInt(parameters[parameters.length-1]);
+        }
+        else
+        {
+            return 1;
+        }
     }
 }
