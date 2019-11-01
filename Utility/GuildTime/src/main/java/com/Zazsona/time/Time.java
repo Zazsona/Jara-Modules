f7b2f004c984ba969ce8c3e535320b718f724a4c
Zazsona <40435178+Zazsona@users.noreply.github.com>
2019-11-01 22:17:21 +0000

    Allow user to specify the timezone to check in GuildTime.

--- Utility/GuildTime/src/main/java/com/Zazsona/time/Time.java
+++ Utility/GuildTime/src/main/java/com/Zazsona/time/Time.java
@@ -6,35 +6,56 @@ import module.Command;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
+import java.time.DateTimeException;
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.zone.ZoneRulesException;
 
 public class Time extends Command
 {
     @Override
     public void run(GuildMessageReceivedEvent msgEvent, String... parameters)
     {
-        OffsetDateTime time = OffsetDateTime.now(SettingsUtil.getGuildSettings(msgEvent.getGuild().getId()).getTimeZoneId());
-        EmbedBuilder embed = new EmbedBuilder();
-        embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
-        embed.setTitle("==== Timezone ====");
-        StringBuilder descBuilder = new StringBuilder();
-        descBuilder.append("**ID:** UTC (").append(time.getOffset()).append(")\n");
-        descBuilder.append("**Time:** ").append(getTime(time)).append("\n");
-        descBuilder.append("**Date:** ").append(time.getYear()).append("/").append(time.getMonthValue()).append("/").append(time.getDayOfMonth()).append("\n");
-        descBuilder.append("**Weekday:** ").append(getDayOfWeekPretty(time.getDayOfWeek().getValue()));
-        embed.setDescription(descBuilder.toString());
-        msgEvent.getChannel().sendMessage(embed.build()).queue();
+        try
+        {
+            ZonedDateTime time;
+            if (parameters.length <= 1)
+                time = ZonedDateTime.now(SettingsUtil.getGuildSettings(msgEvent.getGuild().getId()).getTimeZoneId());
+            else
+                time = ZonedDateTime.now(ZoneId.of(parameters[1]));
+
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+            embed.setTitle("==== Timezone ====");
+            StringBuilder descBuilder = new StringBuilder();
+            descBuilder.append("**ID:** UTC (").append(time.getOffset()).append(")\n");
+            descBuilder.append("**Time:** ").append(getTime(time)).append("\n");
+            descBuilder.append("**Date:** ").append(time.getYear()).append("/").append(time.getMonthValue()).append("/").append(time.getDayOfMonth()).append("\n");
+            descBuilder.append("**Weekday:** ").append(getDayOfWeekPretty(time.getDayOfWeek().getValue()));
+            embed.setDescription(descBuilder.toString());
+            msgEvent.getChannel().sendMessage(embed.build()).queue();
+        }
+        catch (DateTimeException e)
+        {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setColor(CmdUtil.getHighlightColour(msgEvent.getGuild().getSelfMember()));
+            embed.setTitle("==== Timezone ====");
+            embed.setDescription("Invalid timezone.");
+            msgEvent.getChannel().sendMessage(embed.build()).queue();
+        }
+
     }
 
-    private String getTime(OffsetDateTime utc)
+    private String getTime(ZonedDateTime zdt)
     {
         StringBuilder clockBuilder = new StringBuilder();
-        String hour = (utc.getHour() < 10) ? "0"+utc.getHour() : String.valueOf(utc.getHour());
-        String minute = (utc.getMinute() < 10) ? "0"+utc.getMinute() : String.valueOf(utc.getMinute());
+        String hour = (zdt.getHour() < 10) ? "0"+zdt.getHour() : String.valueOf(zdt.getHour());
+        String minute = (zdt.getMinute() < 10) ? "0"+zdt.getMinute() : String.valueOf(zdt.getMinute());
 
-        int ampmHour = (utc.getHour() < 12) ? utc.getHour() : utc.getHour()-12;
+        int ampmHour = (zdt.getHour() < 12) ? zdt.getHour() : zdt.getHour()-12;
         ampmHour = (ampmHour == 0) ? 12 : ampmHour;
-        String ampm = (utc.getHour() < 12) ? "AM" : "PM";
+        String ampm = (zdt.getHour() < 12) ? "AM" : "PM";
 
         clockBuilder.append(hour).append(":").append(minute).append(" (").append(ampmHour).append(":").append(minute).append(ampm).append(")");
         return clockBuilder.toString();
