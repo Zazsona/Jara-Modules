211e55541c4c17d655f5e649c779f0eb045bf946
Zazsona <40435178+Zazsona@users.noreply.github.com>
2019-11-07 12:01:05 +0000

    Make IsItWednesdayMyDudes respect guild time.

--- Toys/IsItWednesdayMyDudes/src/main/java/com/Zazsona/IsItWednesdayMyDudes/IsItWednesdayMyDudes.java
+++ Toys/IsItWednesdayMyDudes/src/main/java/com/Zazsona/IsItWednesdayMyDudes/IsItWednesdayMyDudes.java
@@ -1,16 +1,17 @@
 package com.Zazsona.IsItWednesdayMyDudes;
 
+import configuration.SettingsUtil;
 import module.ModuleCommand;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-
-import java.time.LocalDate;
+import java.time.ZonedDateTime;
 
 public class IsItWednesdayMyDudes extends ModuleCommand
 {
     @Override
     public void run(GuildMessageReceivedEvent msgEvent, String... parameters)
     {
-        if (LocalDate.now().getDayOfWeek().getValue() == 3)
+        ZonedDateTime guildTime = SettingsUtil.getGuildSettings(msgEvent.getGuild().getId()).getZonedDateTime();
+        if (guildTime.getDayOfWeek().getValue() == 3)
         {
             msgEvent.getChannel().sendMessage("https://i.imgur.com/n7I7cKp.jpg").complete();
         }
